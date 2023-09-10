7596836adcfbbd3d5dcec26758d07c60e142bd73
gilmanchik <gilma00@bk.ru>
2023-09-11 00:31:17 +0500

    2nd commit добавил стили шаблоны url настроил

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Women(models.Model):
+    title = models.CharField(
+        max_length=32,
+        verbose_name='Заголовок'
+    )
+
+    content = models.TextField(
+        verbose_name='Текст'
+    )
+
+    photo = models.ImageField(
+        upload_to='photos/%Y/%m/%d',
+        verbose_name='Фото'
+    )
+
+    time_create = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Время создания'
+    )
+
+    time_update = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Время обновления'
+    )
+
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Публикация'
+    )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,6 +1,40 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
 
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'addpage'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'}
+]
+
 
 def index(request):
-    pass
+    posts = Women.objects.all()
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная'
+    }
+    return render(request, 'women/index.html', context=context)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'title': 'Описание'})
+
+
+def addpage(request):
+    return HttpResponse('addpage')
+
+
+def contact(request):
+    return HttpResponse('contact')
+
+
+def login(request):
+    return HttpResponse('login')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'post {post_id}')
