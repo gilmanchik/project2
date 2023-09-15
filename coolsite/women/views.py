1b6d6d15cc370b3dccc87909ca5c3bf26f28b612
gilmanchik <gilma00@bk.ru>
2023-09-15 08:30:35 +0500

    добавил авторизацию пользователей

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,12 +1,12 @@
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import *
-from .models import *
 from .utils import *
 
 
@@ -67,8 +67,8 @@ def contact(request):
     return HttpResponse('contact')
 
 
-def login(request):
-    return HttpResponse('login')
+# def login(request):
+#     return HttpResponse('login')
 
 
 # def show_post(request, post_slug):
@@ -135,3 +135,26 @@ class RegisterUser(DataMixin, CreateView):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Регистрация')
         return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
